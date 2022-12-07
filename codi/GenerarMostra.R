53a9cf813e1dbe5cd98628dbcd80cb4b22b52c5a
Ramon Puig Treserres <raypuig@gmail.com>
2022-12-07 12:09:31 +0100

    ACT

--- codi/GenerarMostra.R
+++ codi/GenerarMostra.R
@@ -3,13 +3,14 @@ library("FormatGe")
 
 #E:\GoDrive\CIBERDEM\DADES\GLOBALDIAB2
 
-generar_mostra_fitxers(
-  directori = "../DADES/GLOBALDIAB2",
-  fitxer_poblacio = "GLOBALDIAB2_entregable_poblacio_20221118.rds",
-  mida_mostra = 10000,
-  prefix = "test",
-  directori_test = "mostra",
-  idp = "idp",
-  llavor = "1234"
-)
+#generar_mostra_fitxers(
+#  directori = "../DADES/GLOBALDIAB2",
+#  fitxer_poblacio = "GLOBALDIAB2_entregable_poblacio_20221118.rds",
+#  mida_mostra = 10000,
+#  prefix = "test",
+#  directori_test = "mostra",
+#  idp = "idp",
+#  llavor = "1234"
+#)
 
+devtools::install_github("USR-DAPCAT/TemplateUEMCID")
